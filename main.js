c5b99ce156f8a94f0781f786acb438e3cd20e518
Francisco Rocha <fa676533@gmail.com>
2026-01-18 01:17:22 -0600

    proyecto finalizado

---
+++ main.js
@@ -0,0 +1,44 @@
+// Selección de elementos
+const inputForm = document.querySelector('.container__form--input');
+const buttonNotify = document.querySelector('.container__form--button');
+const errorMessage = document.querySelector('.container__form--message');
+
+// Inicialización: deshabilitar botón al cargar
+buttonNotify.disabled = true;
+
+// Event listener para validar el input
+inputForm.addEventListener('input', (e) => {
+    const inputValue = e.target.value;
+    //Regex del email
+    const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if(emailRegex.test(inputValue.trim())){
+        buttonNotify.disabled = false;
+        errorMessage.classList.add('hidden')
+        inputForm.classList.remove('container__form--input-invalid')
+        inputForm.classList.add('container__form--input-success')
+    }else{
+        buttonNotify.disabled = true;
+        errorMessage.classList.remove('hidden')
+        inputForm.classList.add('container__form--input-invalid')
+        inputForm.classList.remove('container__form--input-success')
+    }
+});
+
+// Event listener para el boton
+buttonNotify.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    //Obtner el valor del input
+    const valueInput = inputForm.value;
+    //Regex del email
+    const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if(valueInput.trim() === '' || !emailRegex.test(valueInput)){
+        console.log('Email no valido')
+    } else {
+        console.log('Email valido')
+    }
+
+})
+
